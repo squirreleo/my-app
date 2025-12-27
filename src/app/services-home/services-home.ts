49245341a7eba488adf62d4a71c4a0e66cc74cc1
squirreleo <angeladowns@yahoo.com>
2025-12-27 12:11:18 -0600

    Direcitive highlight file added to services

--- src/app/services-home/services-home.ts
+++ src/app/services-home/services-home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HighlightDirective } from './highlight';
 
 @Component({
   selector: 'app-services-home',
-  imports: [],
+  standalone: true,
+  imports: [HighlightDirective],
   templateUrl: './services-home.html',
   styleUrl: './services-home.css',
 })
