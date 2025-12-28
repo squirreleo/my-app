f6a4558154587839c6a71ab6271e0ce13c2233a4
squirreleo <angeladowns@yahoo.com>
2025-12-28 13:56:50 -0600

    disabled send button with property binding

--- src/app/contact-home/contact-home.ts
+++ src/app/contact-home/contact-home.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-home',
+  standalone: true,
   imports: [],
   templateUrl: './contact-home.html',
   styleUrl: './contact-home.css',
 })
-export class ContactHome {
+export class ContactHome implements OnInit {
+  isUnchanged = true;
 
+  constructor () {}
+
+  ngOnInit(): void {}
 }
